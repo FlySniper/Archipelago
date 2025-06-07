14b4322cc5e6d571613716b6c4cd65dadc8ce890
Thomas Barlow <github@mysterymayhem.co.uk>
2025-06-08 00:08:39 +0100

    Add the client to __init__.py

--- worlds/lsw_tcs/__init__.py
+++ worlds/lsw_tcs/__init__.py
@@ -1,8 +1,19 @@
 from worlds.AutoWorld import WebWorld, World
+from worlds.LauncherComponents import components, Component, launch_subprocess, Type
 
 from . import constants
 from .items import ITEM_NAME_TO_ID
 from .options import LegoStarWarsTCSOptions
+
+
+def launch_client():
+    from .client import launch
+    launch_subprocess(launch, name="LegoStarWarsTheCompleteSagaClient")
+
+
+components.append(Component("Lego Star Wars: The Complete Saga Client",
+                            func=launch_client,
+                            component_type=Type.CLIENT))
 
 
 class LegoStarWarsTCSWebWorld(WebWorld):
