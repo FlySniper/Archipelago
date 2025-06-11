170fcb4f42ca134fd31cc53d185b0bccc050b5bc
Thomas Barlow <github@mysterymayhem.co.uk>
2025-06-11 20:54:32 +0100

    BonusLevelCompletionChecker comments

--- worlds/lsw_tcs/client/location_checkers/bonus_level_completion.py
+++ worlds/lsw_tcs/client/location_checkers/bonus_level_completion.py
@@ -4,7 +4,12 @@ from ...locations import LOCATION_NAME_TO_ID
 
 
 class BonusLevelCompletionChecker:
-    # Anakin's Flight and A New Hope support Free Play, but the rest are Story mode only.
+    """
+    Check if the player has completed a bonus level by reading the completion byte of each bonus level that has not
+    already been completed according to the server.
+    """
+    # Anakin's Flight and A New Hope support Free Play, but the rest are Story mode only, for now, this class only
+    # checks for story completion.
     remaining_story_completion_checks: dict[MemoryAddress, ApLocationId]
 
     def __init__(self):
@@ -13,12 +18,20 @@ class BonusLevelCompletionChecker:
         }
 
     async def check_completion(self, ctx: TCSContext, new_location_checks: list[int]):
+        # As location checks get sent, the remaining bytes to gets reduced.
         updated_remaining_story_completion_checks = {}
         for address, ap_id in self.remaining_story_completion_checks.items():
+            # Memory reads are assumed to be the slowest part
             if ap_id in ctx.checked_locations:
+                # By skipping the location, it will not be added to the update dictionary, so will not be checked in the
+                # future.
                 continue
+            # It seems that the value is always `1` for a completed bonus and `0` otherwise. The client checks
+            # truthiness in case it is possible that other bits could be set.
             if ctx.read_uchar(address):
                 # The bonus has been completed, or viewed in the case of the Indiana Jones trailer.
                 new_location_checks.append(ap_id)
+            # Even if the location is being sent, it is still added to the updated dictionary in case the client loses
+            # connection from the server.
             updated_remaining_story_completion_checks[address] = ap_id
         self.remaining_story_completion_checks = updated_remaining_story_completion_checks
